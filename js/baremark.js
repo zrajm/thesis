13fb13b13ac4ce70eb5c9c745e0194f57eb31ad6
zrajm <zrajm@klingonska.org>
2024-09-24 20:52:14 +0200

    Upgraded to Baremark with better <p> handling

--- js/baremark.js
+++ js/baremark.js
@@ -21,6 +21,6 @@
 		[/!\[(\n?.+?(\n.+?)*?\n?)\]\( *\n? *([^\n ]+) *\n? *\)/g,'<img src="$3" alt="$1">'],
 		[/\[(\n?.+?(\n.+?)*?\n?)\]\( *\n? *([^\n ]+) *\n? *\)/g,'<a href="$3">$1</a>'],
 		[/\[(\n?.+?(\n.+?)*?\n?)\](?:\[(\n?.+?(\n.+?)*?\n?)\])?/g,(w,t,_,n)=>(n=s(n||t),l[n]?`<a href="${l[n][0]}" title="${l[n][1]||''}">${t}</a>`:w)],
-		[/\n\n(.+(\n.+)*)(?=\n\n)/g,(w,m)=>/^<(\/|blockquote|h\d|hr|li|ol|ul|p|pre|table)\b/.test(m)?w:`\n\n<p>${m}</p>`]]
+		[/\n\n(.+(\n.+)*)(?=\n\n)/g,(w,m)=>/^<(\/|address|article|aside|blockquote|details|div|[dou]l|fieldset|fig(caption|ure)|footer|form|h\d|header|hgroup|hr|main|menu|nav|p|pre|(no)?script|search|section|style|table)\b/.test(m)?w:`\n\n<p>${m}</p>`]]
 	w.baremark=x=>x===undefined?r:(l={},r.reduce((a,r)=>a.replace(...r),`\n\n${x}\n\n`).trim())
 })(self)
